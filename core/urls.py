66640b530dff455124d1da30322c3b89a8004b09
Bek <103777571+devvesama@users.noreply.github.com>
2026-03-22 02:16:07 +0500

    postgresql

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include  # Импортируй include!
+from django.contrib.auth.models import User
+import os
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Эта строка говорит Django: "Если адрес начинается с /feedback/, 
-    # ищи дальнейшие инструкции в файле feedback.urls"
-    path('feedback/', include('feedback.urls')), 
-]
+# Скрипт для автоматического создания админа в новой базе
+try:
+    # Проверяем, существует ли уже такой юзер, чтобы не было ошибки
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin', 
+            email='admin@example.com', 
+            password='devve08082002'  # <--- ПИШИ ПАРОЛЬ ТУТ
+        )
+        print("--- СУПЕРЮЗЕР СОЗДАН УСПЕШНО ---")
+except Exception as e:
+    # Если база еще не готова или таблиц нет, просто пропустим
+    print(f"--- Ошибка авто-админа: {e} ---")
