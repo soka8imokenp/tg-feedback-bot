37ba554ce07a2f7bc3e78999e1cbddecf75012ba
devvesama <tokimachinedesu@gmail.com>
2026-03-21 17:55:29 +0500

    real fix for history

--- create_admin.py
+++ create_admin.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 username = 'admin'
-password = 'твой_пароль_тут' # Укажи свой пароль
+password = 'devve08082002' # Укажи свой пароль
 
 user = User.objects.filter(username=username).first()
 
