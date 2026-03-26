34f31a744b970c6d40f6efe02782102179333677
devvesama <tokimachinedesu@gmail.com>
2026-03-26 17:06:43 +0500

    new confirmation panel

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
 from . import views
 
+# Рекомендуется добавить app_name, если проект будет расти
+app_name = 'feedback' 
+
 urlpatterns = [
-    # Главная страница Mini App (Список сообщений)
+    # Главная страница (Список тикетов)
     path('', views.index, name='feedback_index'),
     
     # Отправка новой формы
     path('submit/', views.submit_feedback, name='submit_feedback'),
     
-    # Закрытие тикета
+    # Закрытие тикета (убедитесь, что в views.py функция принимает ticket_id)
     path('close/<int:ticket_id>/', views.close_ticket, name='close_ticket'),
 
     # Ответ в существующий диалог
     path('reply/<int:ticket_id>/', views.reply_ticket, name='reply_ticket'),
 
-    # НОВОЕ: Подгрузка дополнительных тикетов (Пагинация)
-    # name='load_more_tickets' должен совпадать с тем, что мы написали в hx-get шаблона
+    # Подгрузка дополнительных тикетов (AJAX/HTMX)
     path('load-more/', views.load_more_tickets, name='load_more_tickets'),
 ]
